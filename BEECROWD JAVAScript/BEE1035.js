9d61a02ab7f6508a4572ec1a6669d7aba6d29ced
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-08 10:30:41 -0300

    Create BEE1035.js

---
+++ BEECROWD JAVAScript/BEE1035.js
@@ -0,0 +1,12 @@
+var valores = lines.shift().trim().split(" ")
+
+var A = parseInt(valores.shift())
+var B = parseInt(valores.shift())
+var C = parseInt(valores.shift())
+var D = parseInt(valores.shift())
+
+if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 === 0) {
+    console.log("Valores aceitos");
+  } else {
+    console.log("Valores nao aceitos");
+  }
