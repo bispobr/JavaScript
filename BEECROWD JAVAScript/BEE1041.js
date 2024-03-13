968fbdb14f2d94c6604fa2d25702464dd02ff971
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-13 15:31:03 -0300

    Create BEE1041.js

---
+++ BEECROWD JAVAScript/BEE1041.js
@@ -0,0 +1,29 @@
+var valores = lines.shift().trim().split(" ")
+var x = parseFloat(valores.shift())
+var y =  parseFloat(valores.shift())
+if (x > 0){
+    if (y > 0){
+        console.log(`Q1`)
+    } else if (y < 0){
+        console.log(`Q4`)
+    }else{
+        console.log(`Eixo X`)
+    }
+
+} else if (x < 0){
+    if (y > 0 ){
+        console.log(`Q2`)
+    } else if (y < 0){
+        console.log(`Q3`)
+    } else {
+        console.log(`Eixo X`)
+    }
+} else {
+    if (y > 0){
+        console.log(`Eixo Y`)
+    } else if (y < 0){
+        console.log(`Eixo Y`)
+    } else{
+        console.log(`Origem`)
+    }
+}
