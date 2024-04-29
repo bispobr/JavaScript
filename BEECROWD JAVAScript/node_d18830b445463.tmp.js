29fb8256bfd899e16d1173cd9d3c7cdf2228c455
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-29 10:01:10 -0300

    BEE1132

---
+++ BEECROWD JAVAScript/node_d18830b445463.tmp.js
@@ -0,0 +1,19 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var x = parseInt(lines.shift())
+var y = parseInt(lines.shift())
+
+var soma = 0;
+
+if (x> y){
+    [x,y] = [y,x]
+}
+
+for (var c = x; x < y;c++){
+    if (c % 13 != 0)[
+        soma += c
+    ]
+}
+
+console.log(soma)
