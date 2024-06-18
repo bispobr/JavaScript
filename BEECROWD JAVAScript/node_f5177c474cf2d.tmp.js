1fd7f75c472042e27e18a5da50ad0935b9c7a75b
bispobr <156684382+bispobr@users.noreply.github.com>
2024-06-18 11:10:40 -0300

    BEE1165

---
+++ BEECROWD JAVAScript/node_f5177c474cf2d.tmp.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var casos = parseInt(lines.shift())
+
+for (var c = 1;c = casos;c++){
+    console.log(c)
+}
