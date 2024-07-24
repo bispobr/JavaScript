18b7f0e415ed129ac6587afb2e55bc7ebc579fcf
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-24 13:46:24 -0300

    Create BEE1924.js

---
+++ BEECROWD JAVAScript/BEE1924.js
@@ -0,0 +1,11 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+let casos = parseInt(lines.shift());
+
+for (let i = 0; i < casos; i++) {
+  
+  let m = lines.shift()
+ 
+}
+console.log("Ciencia da Computacao");
