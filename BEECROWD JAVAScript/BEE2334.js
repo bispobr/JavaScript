8d48548ec13abfc655919c2d4ea03db1a224b719
bispobr <156684382+bispobr@users.noreply.github.com>
2024-09-03 15:17:33 -0300

    Create BEE2334.js

---
+++ BEECROWD JAVAScript/BEE2334.js
@@ -0,0 +1,24 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+lines.pop();
+
+let p = 0;
+while (p < lines.length) {
+  let entrada = lines[p++].trim();
+  let P = entrada.split("");
+
+  if (entrada !== "0") {
+    let n = P.length - 1;
+    while (P[n] === "0") {
+      P[n--] = "9";
+    }
+    P[n] = (parseInt(P[n]) - 1).toString();
+  }
+
+  if (entrada.length > 1 && P[0] === "0") {
+    P = P.splice(1);
+  }
+
+  console.log(P.join(""));
+}
